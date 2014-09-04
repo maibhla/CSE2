a540f6bff3a39855fe5f01edff83e3ac1aa41889
srk215 <srk215@lehigh.edu>
2014-09-04 00:37:02 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+// Scott Kaplan
+// Homework 1
+// Welcome Class Java Program
+
+// Define a class
+public class  WelcomeClass{
+    
+// add main method
+    public static void main(String[] args) {
+// print the statement
+        System.out.println("   -----------");
+        System.out.println("   | Welcome |");
+        System.out.println("   -----------");
+        System.out.println("   ^  ^  ^  ^  ^  ^");
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println(" |-S--R--K--2--1--5-|");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("   v  v  v  v  v  v");
+        System.out.println("This Summer I worked for a technology company");
+        System.out.println("doing quality assurance on their programs");
+        // end code
+
+    }
+}
